4e1e0adca2ea3c25f933c172c468d0276b711fc1
Lee Ewing <mrlee588@gmail.com>
2026-02-11 09:26:25 -0600

    switch back from paper to spigot

--- v1_21_10/build.gradle.kts
+++ v1_21_10/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:${rootProject.extra["paperApiVersion_1_21_10"]}")
+    compileOnly("org.spigotmc:spigot:1.21.10-R0.1-SNAPSHOT:remapped-mojang")
 
     compileOnly("net.kyori:adventure-api:4.26.1")
     compileOnly("net.kyori:adventure-platform-bukkit:4.4.1")
